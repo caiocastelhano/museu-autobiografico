05997f5d0d0e2d786e31889b5ae97723949a11c4
Caio Castelhano <castelhanoc@gmail.com>
2025-03-19 11:54:40 -0300

    Adiciona Navbar e Footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+    return (
+      <footer className={styles.footer}>
+        <p>Museu Autobiográfico © 2025 | Todos os direitos reservados | Desenvolvido por Caio Castelhano</p>
+      </footer>
+    );
+  }
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <h1 className={styles.navbarTitle}>museu autobiográfico e território leste</h1>
+    </nav>
+  );
+}
