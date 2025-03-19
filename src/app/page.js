4cce03d8924a586b834f99cb46d56fb79a1493a2
Caio Castelhano <castelhanoc@gmail.com>
2025-03-19 18:03:38 -0300

    Corrige a estrutura da home sem o estilo

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,28 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import styles from './styles/Page.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.pageContainer}>
       <Navbar />
 
-      <div>Sobre/About</div>
+      <div className={styles.aboutSection}>Sobre/About</div>
 
-      <div>
-        <div>Museu</div>
-        <div>Autobiográfico</div>
-        <div>Território</div>
-        <div>Leste</div>
-        <div>Corpo</div>
-        <div>Memória</div>
+      <div className={styles.constellation}>
+        <div id="museum" className={styles.section}>Museu</div>
+        <div id="autobiographic" className={styles.section}>Autobiográfico</div>
+        <div id="territory" className={styles.section}>Território</div>
+        <div id="east" className={styles.section}>Leste</div>
+        <div id="body" className={styles.section}>Corpo</div>
+        <div id="memory" className={styles.section}>Memória</div>
+
+        {/* Linhas conectando os elementos */}
+        <div id="line1" className={styles.line}></div>
+        <div id="line2" className={styles.line}></div>
       </div>
 
-      <div>Contato/Contact</div>
+      <div className={styles.contactSection}>Contato/Contact</div>
 
       <Footer />
     </div>
