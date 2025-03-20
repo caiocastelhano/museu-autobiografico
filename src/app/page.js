44d026fc91666073ee8f4de7c04b7973b3907fa3
Caio Castelhano <castelhanoc@gmail.com>
2025-03-20 15:24:51 -0300

    Adicione placeholders para todas as seções da home

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import styles from "./styles/Page.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -8,8 +9,12 @@ export default function Home() {
       <Navbar />
 
       {/* "sobre/about" e "contato/contact" */}
-      <div className={styles.sobre}>sobre/about</div>
-      <div className={styles.contato}>contato/contact</div>
+      <Link href="/#">
+        <div className={styles.sobre}>sobre/about</div>
+      </Link>
+      <Link href="/#">
+        <div className={styles.contato}>contato/contact</div>
+      </Link>
 
       {/* Container central para a constelação */}
       <div className={styles.mainContent}>
@@ -60,118 +65,130 @@ export default function Home() {
           {/* POLÍGONOS e TEXTOS */}
 
           {/* 1. MUSEU */}
-          <g className={styles.hoverText}>
-          <polygon
-            points="95,210 205,210 235,280 175,310 95,280"
-            fill="white"
-            stroke="black"
-            strokeWidth="1"
-          />
-          {/* Pontos nas arestas do polígono, simulando estrelas */}
-          <circle cx="95"  cy="210" r="2" fill="black" />
-          <circle cx="205" cy="210" r="2.5" fill="black" />
-          <circle cx="235" cy="280" r="2" fill="black" />
-          <circle cx="175" cy="310" r="1.5" fill="black" />
-          <circle cx="95"  cy="280" r="3" fill="black" />
-          <text x="161" y="258" className={styles.shapeText} textAnchor="middle">
-            museu
-          </text>
-          </g>
+          <Link href="/#">
+            <g className={styles.hoverText}>
+            <polygon
+              points="95,210 205,210 235,280 175,310 95,280"
+              fill="white"
+              stroke="black"
+              strokeWidth="1"
+            />
+            {/* Pontos nas arestas do polígono, simulando estrelas */}
+            <circle cx="95"  cy="210" r="2" fill="black" />
+            <circle cx="205" cy="210" r="2.5" fill="black" />
+            <circle cx="235" cy="280" r="2" fill="black" />
+            <circle cx="175" cy="310" r="1.5" fill="black" />
+            <circle cx="95"  cy="280" r="3" fill="black" />
+            <text x="161" y="258" className={styles.shapeText} textAnchor="middle">
+              museu
+            </text>
+            </g>
+          </Link>
 
           {/* 2. AUTOBIOGRÁFICO */}
-          <g className={styles.hoverText}>
-          <polygon
-            points="546,94 654,112 672,196 600,232 528,178"
-            fill="white"
-            stroke="black"
-            strokeWidth="1"
-          />
-          {/* Pontos nas arestas do polígono, simulando estrelas */}
-          <circle cx="546" cy="94" r="2" fill="black" />
-          <circle cx="654" cy="112" r="2.5" fill="black" />
-          <circle cx="672" cy="196" r="4" fill="black" />
-          <circle cx="600" cy="232" r="1.5" fill="black" />
-          <circle cx="528" cy="178" r="2" fill="black" />
-          <text x="600" y="162" className={styles.shapeText} textAnchor="middle">
-            autobiográfico
-          </text>
-          </g>
+          <Link href="/#">
+            <g className={styles.hoverText}>
+            <polygon
+              points="546,94 654,112 672,196 600,232 528,178"
+              fill="white"
+              stroke="black"
+              strokeWidth="1"
+            />
+            {/* Pontos nas arestas do polígono, simulando estrelas */}
+            <circle cx="546" cy="94" r="2" fill="black" />
+            <circle cx="654" cy="112" r="2.5" fill="black" />
+            <circle cx="672" cy="196" r="4" fill="black" />
+            <circle cx="600" cy="232" r="1.5" fill="black" />
+            <circle cx="528" cy="178" r="2" fill="black" />
+            <text x="600" y="162" className={styles.shapeText} textAnchor="middle">
+              autobiográfico
+            </text>
+            </g>
+          </Link>
 
           {/* 3. CORPO */}
-          <g className={styles.hoverText}>
-          <polygon
-            points="870,65 930,80 945,125 885,155 840,110"
-            fill="white"
-            stroke="black"
-            strokeWidth="1"
-          />
-          {/* Pontos nas arestas do polígono, simulando estrelas */}
-          <circle cx="870"  cy="65" r="2" fill="black" />
-          <circle cx="930" cy="80" r="2.5" fill="black" />
-          <circle cx="945" cy="124" r="4" fill="black" />
-          <circle cx="885" cy="155" r="1.5" fill="black" />
-          <circle cx="840"  cy="110" r="3" fill="black" />
-          <text x="895" y="108" className={styles.shapeText} textAnchor="middle">
-            corpo
-          </text>
-          </g>
+          <Link href="/#">
+            <g className={styles.hoverText}>
+            <polygon
+              points="870,65 930,80 945,125 885,155 840,110"
+              fill="white"
+              stroke="black"
+              strokeWidth="1"
+            />
+            {/* Pontos nas arestas do polígono, simulando estrelas */}
+            <circle cx="870"  cy="65" r="2" fill="black" />
+            <circle cx="930" cy="80" r="2.5" fill="black" />
+            <circle cx="945" cy="124" r="4" fill="black" />
+            <circle cx="885" cy="155" r="1.5" fill="black" />
+            <circle cx="840"  cy="110" r="3" fill="black" />
+            <text x="895" y="108" className={styles.shapeText} textAnchor="middle">
+              corpo
+            </text>
+            </g>
+          </Link>
 
           {/* 4. LESTE */}
-          <g className={styles.hoverText}>
-          <polygon
-            points="970,265 1030,280 1045,325 985,355 940,310"
-            fill="white"
-            stroke="black"
-            strokeWidth="1"
-          />
-          {/* Pontos nas arestas do polígono, simulando estrelas */}
-          <circle cx="970"  cy="265" r="2" fill="black" />
-          <circle cx="1030" cy="280" r="2" fill="black" />
-          <circle cx="1045" cy="325" r="3" fill="black" />
-          <circle cx="985" cy="355" r="2.5" fill="black" />
-          <circle cx="940"  cy="310" r="4" fill="black" />
-          <text x="995" y="307" className={styles.shapeText} textAnchor="middle">
-            leste
-          </text>
-          </g>
+          <Link href="/#">
+            <g className={styles.hoverText}>
+            <polygon
+              points="970,265 1030,280 1045,325 985,355 940,310"
+              fill="white"
+              stroke="black"
+              strokeWidth="1"
+            />
+            {/* Pontos nas arestas do polígono, simulando estrelas */}
+            <circle cx="970"  cy="265" r="2" fill="black" />
+            <circle cx="1030" cy="280" r="2" fill="black" />
+            <circle cx="1045" cy="325" r="3" fill="black" />
+            <circle cx="985" cy="355" r="2.5" fill="black" />
+            <circle cx="940"  cy="310" r="4" fill="black" />
+            <text x="995" y="307" className={styles.shapeText} textAnchor="middle">
+              leste
+            </text>
+            </g>
+          </Link>
 
           {/* 5. TERRITÓRIO */}
-          <g className={styles.hoverText}>
-          <polygon
-            points="500,368 560,383 575,428 515,458 470,413"
-            fill="white"
-            stroke="black"
-            strokeWidth="1"
-          />
-          {/* Pontos nas arestas do polígono, simulando estrelas */}
-          <circle cx="500"  cy="368" r="2" fill="black" />
-          <circle cx="560" cy="383" r="2.5" fill="black" />
-          <circle cx="575" cy="428" r="4" fill="black" />
-          <circle cx="515" cy="458" r="1.5" fill="black" />
-          <circle cx="470"  cy="413" r="3" fill="black" />
-          <text x="524" y="410" className={styles.shapeText} textAnchor="middle">
-            território
-          </text>
-          </g>
+          <Link href="/#">
+            <g className={styles.hoverText}>
+            <polygon
+              points="500,368 560,383 575,428 515,458 470,413"
+              fill="white"
+              stroke="black"
+              strokeWidth="1"
+            />
+            {/* Pontos nas arestas do polígono, simulando estrelas */}
+            <circle cx="500"  cy="368" r="2" fill="black" />
+            <circle cx="560" cy="383" r="2.5" fill="black" />
+            <circle cx="575" cy="428" r="4" fill="black" />
+            <circle cx="515" cy="458" r="1.5" fill="black" />
+            <circle cx="470"  cy="413" r="3" fill="black" />
+            <text x="524" y="410" className={styles.shapeText} textAnchor="middle">
+              território
+            </text>
+            </g>
+          </Link>
 
           {/* 6. MEMÓRIA */}
-          <g className={styles.hoverText}>
-          <polygon
-            points="260,365 340,365 360,435 300,465 260,435"
-            fill="white"
-            stroke="black"
-            strokeWidth="1"
-          />
-          {/* Pontos nas arestas do polígono, simulando estrelas */}
-          <circle cx="260"  cy="365" r="3" fill="black" />
-          <circle cx="340" cy="365" r="2" fill="black" />
-          <circle cx="360" cy="435" r="3.5" fill="black" />
-          <circle cx="300" cy="465" r="1.5" fill="black" />
-          <circle cx="260" cy="435" r="2" fill="black" />
-          <text x="304" y="413" className={styles.shapeText} textAnchor="middle">
-            memória
-          </text>
-          </g>
+          <Link href="/#">
+            <g className={styles.hoverText}>
+            <polygon
+              points="260,365 340,365 360,435 300,465 260,435"
+              fill="white"
+              stroke="black"
+              strokeWidth="1"
+            />
+            {/* Pontos nas arestas do polígono, simulando estrelas */}
+            <circle cx="260"  cy="365" r="3" fill="black" />
+            <circle cx="340" cy="365" r="2" fill="black" />
+            <circle cx="360" cy="435" r="3.5" fill="black" />
+            <circle cx="300" cy="465" r="1.5" fill="black" />
+            <circle cx="260" cy="435" r="2" fill="black" />
+            <text x="304" y="413" className={styles.shapeText} textAnchor="middle">
+              memória
+            </text>
+            </g>
+          </Link>
         </svg>
       </div>
 
