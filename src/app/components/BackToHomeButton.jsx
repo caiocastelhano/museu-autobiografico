17b134c741c5765b3247ac2aee15ef11fcfe887d
Caio Castelhano <castelhanoc@gmail.com>
2025-04-19 18:21:55 -0300

    Implementa acessibilidade em backtohomebutton

--- src/app/components/BackToHomeButton.jsx
+++ src/app/components/BackToHomeButton.jsx
@@ -1,10 +1,14 @@
-// components/BackToHomeButton.jsx
 import Link from "next/link";
 import styles from "../styles/BackToHomeButton.module.css";
 
 export default function BackToHomeButton() {
   return (
-    <Link href="/" className={styles.backButton}>
+    <Link
+      href="/"
+      className={styles.backButton}
+      aria-label="Voltar para a página inicial"
+      title="Voltar para a página inicial"
+    >
       ⟵
     </Link>
   );
