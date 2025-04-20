7fb10b27a1500b4c967eb820b9e01bd52b16dbbb
Caio Castelhano <castelhanoc@gmail.com>
2025-04-20 13:33:33 -0300

    Adiciona estrutura da página Sobre

--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -1,9 +1,26 @@
-export default function Sobre() {
-    return (
-      <div style={{ padding: "80px 20px", textAlign: "center" }}>
-        <h1>Seção Sobre</h1>
-        <p>Em breve: conteúdo desta página.</p>
-      </div>
-    );
-  }
-  
+"use client";
+
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+import BackToHomeButton from "@/app/components/BackToHomeButton";
+import TopMenu from "@/app/components/TopMenu";
+import styles from "@/app/sobre/Sobre.module.css";
+// import Image from "next/image";
+// import Link from "next/link";
+// import FadeInSection from "@/app/components/FadeInSection";
+
+export default function SobrePage() {
+  return (
+    <div className={styles.pageContainer}>
+      <Navbar />
+      <TopMenu />
+
+      <main className={styles.mainContent}>
+        <h1 className={styles.visuallyHidden}>Página Sobre</h1>
+      </main>
+
+      <BackToHomeButton />
+      <Footer />
+    </div>
+  );
+}
